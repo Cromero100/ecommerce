c3a88a6b159efd8458809833a471e85c9f8091ae
Cristian Romero <cfromerop@ufpso.edu.co>
2025-02-01 13:47:25 -0500

    correccion

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ProductList } from "./ProductList";
-import "./Home.css";
+import "./home.css";
 import { Link } from "react-router";
 
 export const Home = () => {
