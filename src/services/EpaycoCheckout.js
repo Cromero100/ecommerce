6255bd8ee20972375ada5be4f8635ac029065a7c
Cristian Romero <cfromerop@ufpso.edu.co>
2025-02-01 20:14:23 -0500

    correccion compra

--- src/services/EpaycoCheckout.js
+++ src/services/EpaycoCheckout.js
@@ -15,25 +15,27 @@ const handlePayment = (selectedProducts) => {
     (acc, item) => acc + item.price * (item.quantity || 1),
     0
   );
-  // const taxBase = totalAmount * 0.84; // Asumiendo que el 84% del total es base imponible
-  // const tax = totalAmount * 0.16; // IVA del 16%
+  const taxBase = totalAmount * 0.84; // Asumiendo que el 84% del total es base imponible
+  const tax = totalAmount * 0.16; // IVA del 16%
 
   // Datos de la transacción
   const data = {
-    name: selectedProducts.map((item) => item.name).join(", "), // Nombres de productos
-    description: selectedProducts.map((item) => item.name).join(" - "), // Descripción
+    name: selectedProducts.map((item) => item.title).join(", "), // Nombres de productos
+    description: selectedProducts.map((item) => item.title).join(" - "), // Descripción
     invoice: `FAC-${Math.floor(Math.random() * 1000000)}`, // Generar un número de factura aleatorio
     currency: "cop",
-    amount: totalAmount.toFixed(2),
-    // tax_base: taxBase.toFixed(2),
-    // tax: tax.toFixed(2),
+    amount: 5000.0,
+    tax_base: taxBase.toFixed(2),
+    tax: tax.toFixed(2),
     tax_ico: "0", // Si hay otros impuestos, agrégalos aquí
     country: "co",
     lang: "es",
     external: "true",
     confirmation: "http://localhost:8000/confirmacion", // URL backend
     response: "http://localhost:5173/respuesta",
-    //    address_billing: "Carrera 19 numero 14 91",
+    //Atributos cliente
+    name_billing: "Jhon Doe",
+    address_billing: "Carrera 19 numero 14 91",
     type_doc_billing: "cc",
     mobilephone_billing: "3050000000",
     number_doc_billing: "100000000",
